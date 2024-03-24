a99fce8ba00fe6e7e4d3ba4b00fcb94b54daf634
MaxiToscano <maxi.toscano93@gmail.com>
2024-03-23 21:43:12 -0300

    Avance práctica 2, punto 2 (algunos ejs imcompletos)

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -8,6 +8,15 @@ daUnoSiCumple :: Bool -> Int
 daUnoSiCumple True = 1
 daUnoSiCumple _ = 0
 
+agregarSi :: a -> Bool -> [a] -> [a]
+agregarSi x True l = x : l
+agregarSi _ _ l = l
+
+maxDelPar :: (Int, Int) -> Int 
+maxDelPar (n, m) = if n > m 
+                    then n 
+                    else m
+
 -- PUNTO 1: Recursión sobre listas
 
 --Defina las siguientes funciones utilizando recursión estructural sobre listas, salvo que se indique lo contrario:
@@ -61,7 +70,7 @@ pertenece e (x:xs) = e == x || pertenece e xs
 apariciones e [] = 0
 aparaciones e (x:xs) = daUnoSiCumple (e == x) + apariciones e xs-}
 
---DUDA: no me sale con la subtarea
+--DUDA: no me funciona con la subtarea
 
 apariciones :: Eq a => a -> [a] -> Int
 apariciones e []     = 0
@@ -70,35 +79,121 @@ apariciones e (x:xs) = if e == x
                         else apariciones e xs
 
 --9. 
-losMenoresA :: Int -> [Int] -> [Int]
+{-losMenoresA :: Int -> [Int] -> [Int]
 --Dados un número n y una lista xs, devuelve todos los elementos de xs que son menores a n.
 losMenoresA n [] = []
 losMenoresA n (x:xs) = if x < n 
                         then x : losMenoresA n xs
                         else losMenoresA n xs
+-}
+
+losMenoresA :: Int -> [Int] -> [Int]
+--Dados un número n y una lista xs, devuelve todos los elementos de xs que son menores a n.
+losMenoresA n [] = []
+losMenoresA n (x:xs) = agregarSi x (x < n) (losMenoresA n xs)
 
 
 --10. 
+{-
 lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
 --Dados un número n y una lista de listas, devuelve la lista de aquellas listas que tienen más de n elementos.
 lasDeLongitudMayorA n [] = []
 lasDeLongitudMayorA n (x:xs) = if (length x) > n
                                 then x : lasDeLongitudMayorA n xs
                                 else lasDeLongitudMayorA n xs
-
-{-11. agregarAlFinal :: [a] -> a -> [a]
-Dados una lista y un elemento, devuelve una lista con ese elemento agregado al nal de la
-lista.
-12. agregar :: [a] -> [a] -> [a]
-Dadas dos listas devuelve la lista con todos los elementos de la primera lista y todos los
-elementos de la segunda a continuación. Denida en Haskell como (++).
-13. reversa :: [a] -> [a]
-Dada una lista devuelve la lista con los mismos elementos de atrás para adelante. Denida
-en Haskell como reverse.
-14. zipMaximos :: [Int] -> [Int] -> [Int]
-Dadas dos listas de enteros, devuelve una lista donde el elemento en la posición n es el
-máximo entre el elemento n de la primera lista y de la segunda lista, teniendo en cuenta que
-las listas no necesariamente tienen la misma longitud.
-15. elMinimo :: Ord a => [a] -> a
-Dada una lista devuelve el mínimo
 -}
+
+lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
+--Dados un número n y una lista de listas, devuelve la lista de aquellas listas que tienen más de n elementos.
+lasDeLongitudMayorA n [] = []
+lasDeLongitudMayorA n (x:xs) = agregarSi x (length x > n) (lasDeLongitudMayorA n xs)
+
+
+--11. 
+agregarAlFinal :: [a] -> a -> [a]
+--Dados una lista y un elemento, devuelve una lista con ese elemento agregado al final de la lista.
+agregarAlFinal [] e = e : []
+agregarAlFinal (x:xs) e = x : agregarAlFinal xs e 
+
+
+agregarAlFinal' :: [a] -> a -> [a]
+--Dados una lista y un elemento, devuelve una lista con ese elemento agregado al final de la lista.
+agregarAlFinal' [] e = e : []
+agregarAlFinal' l  e = l ++ [e]
+
+
+--12. 
+agregar :: [a] -> [a] -> [a]
+{-Dadas dos listas devuelve la lista con todos los elementos de la primera lista y todos los
+elementos de la segunda a continuación. Definida en Haskell como (++).-}
+agregar [] xs = xs
+agregar (x:xs) ys = x: agregar xs ys
+
+--13. 
+{-reversa :: [a] -> [a]
+--Dada una lista devuelve la lista con los mismos elementos de atrás para adelante. 
+--Definida en Haskell como reverse.
+reversa [] = []
+reversa (x:xs) = -}
+
+--14. 
+zipMaximos :: [Int] -> [Int] -> [Int]
+{-Dadas dos listas de enteros, devuelve una lista donde el elemento en la posición n es el
+máximo entre el elemento n de la primera lista y de la segunda lista, teniendo en cuenta que
+las listas no necesariamente tienen la misma longitud.-}
+zipMaximos [] ns = ns 
+zipMaximos ns [] = ns
+zipMaximos (n:ns) (m:ms) = maxDelPar (n,m) : zipMaximos ns ms
+
+--15. 
+{-elMinimo :: Ord a => [a] -> a
+--Dada una lista devuelve el mínimo
+elMinimo [] = []
+elMinimo (x:xs) = -}
+
+
+-- /////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- PUNTO 2: Recursión sobre números
+
+--DeFIna las siguientes funciones utilizando recursión sobre números enteros, salvo que se indique lo contrario:
+
+--1. 
+factorial :: Int -> Int
+{-Dado un número n se devuelve la multiplicación de este número y todos sus anteriores hasta
+llegar a 0. Si n es 0 devuelve 1. La función es parcial si n es negativo.-}
+--PRECOND: n no es negativo 
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+--2. 
+cuentaRegresiva :: Int -> [Int]
+{-Dado un número n devuelve una lista cuyos elementos sean los números comprendidos entre
+n y 1 (incluidos). Si el número es inferior a 1, devuelve la lista vacía.-}
+cuentaRegresiva n = if n < 1 
+                    then []
+                    else n : cuentaRegresiva (n-1)
+
+--3. 
+repetir :: Int -> a -> [a]
+--Dado un número n y un elemento e devuelve una lista en la que el elemento e repite n veces.
+repetir 0 e = []
+repetir n e = e : repetir (n-1) e
+
+
+--4. 
+losPrimeros :: Int -> [a] -> [a]
+{-Dados un número n y una lista xs, devuelve una lista con los n primeros elementos de xs.
+Si la lista es vacía, devuelve una lista vacía.-}
+losPrimeros _ [] = []
+losPrimeros 0 _  = []
+losPrimeros n (x:xs) = x : losPrimeros (n-1) xs
+
+
+--5. 
+sinLosPrimeros :: Int -> [a] -> [a]
+{-Dados un número n y una lista xs, devuelve una lista sin los primeros n elementos de lista
+recibida. Si n es cero, devuelve la lista completa-}
+sinLosPrimeros _ [] = []
+sinLosPrimeros 0 xs = xs 
+--sinLosPrimeros n (x:xs) = 
